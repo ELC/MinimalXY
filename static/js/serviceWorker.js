51549d7165bae594647a7f933369ee6dc9109e00
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-10-10 22:07:25 -0300

    :bug: Fix Notifications

--- static/js/serviceWorker.js
+++ static/js/serviceWorker.js
@@ -1,32 +1,29 @@
 // Extracted from https://github.com/GoogleChromeLabs/sw-precache/blob/master/demo/app/js/service-worker-registration.js#L27
+// Modified from https://serviceworke.rs/offline-status.html
 
 if ('serviceWorker' in navigator) {
 
     window.addEventListener('load', function () {
 
-        navigator.serviceWorker.register('/sw.js').then((reg) => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/'}).then((reg) => {
 
-            reg.onupdatefound = () => {
-                var installingWorker = reg.installing;
+            if (reg.active && reg.active.state === 'activated'){
+                launch_toast('Remember this site also works offline!');
+            }
 
-                installingWorker.onstatechange = () => {
-                    switch (installingWorker.state) {
-                        case 'installed':
-                        case 'activated':
-                            if (navigator.serviceWorker.controller) {
-                                launch_toast('Reload to get updated content.');
-                            } else {
-                                launch_toast('Content is now available offline!');
-                            }
-                            break;
-                        case 'redundant':
-                            console.error('The installing service worker became redundant.');
-                            break;
-                    }
-                };
-            };
         }).catch((e) => {
             console.error('Error during service worker registration:', e);
         });
+
+        navigator.serviceWorker.addEventListener('controllerchange', (event) => {
+
+            navigator.serviceWorker.controller.addEventListener('statechange', (e) => {
+                if (e.currentTarget.state === 'activated') {   
+                    launch_toast('Content is now available offline!');
+                }
+            });
+        });
     });
 }
+
+   
